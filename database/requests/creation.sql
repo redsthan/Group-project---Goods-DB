ab946927333a62c08d877ee58eec826fb672ead2
redsthan <rousseaunathan84@gmail.com>
2024-02-04 13:10:53 +0100

    new tables

--- database/requests/creation.sql
+++ database/requests/creation.sql
@@ -10,23 +10,24 @@ CREATE TABLE IF NOT EXISTS products (
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     pseudo VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     description TEXT,
     picture BLOB
 );
 
 CREATE TABLE IF NOT EXISTS basket (
-    user_id INTEGER,
-    product_id INTEGER,
-    quantity INTEGER,
+    user_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(product_id) REFERENCES products(id)
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-    user_id INTEGER,
-    product_id INTEGER,
-    quantity INTEGER,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
