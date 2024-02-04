35253cb40ddff4c58a753ffb034d0cbd24efb91e
redsthan <rousseaunathan84@gmail.com>
2024-02-04 12:41:46 +0100

    Update creation.sql

--- database/requests/creation.sql
+++ database/requests/creation.sql
@@ -5,4 +5,28 @@ CREATE TABLE IF NOT EXISTS products (
     price DECIMAL(10, 2),
     quantity INTEGER,
     illustration BLOB
-)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    pseudo VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    picture BLOB
+);
+
+CREATE TABLE IF NOT EXISTS basket (
+    user_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    user_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
